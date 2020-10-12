ab2d12c736e8f0880564df5c40652addbefb9fe5
Jaroslav <jklumpler@centrum.cz>
2020-10-12 17:10:45 +0100

    working version of NW Price calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nw-tools';
+  @Input() zax = 750;
+  @Input() zenPrice = 100;
+  @Input() sellPrice = 0;
+  amountAd = 0;
+  breakEvenAd = 0;
+  profit = 0;
+  profitPct = 0;
+
+  calculate(): void {
+    this.amountAd = this.zenPrice * this.zax;
+    this.breakEvenAd = Math.ceil(this.amountAd / 0.9);
+    this.profit = Math.floor(this.sellPrice * 0.9) - this.amountAd;
+    this.profitPct = this.profit / this.amountAd * 100.0;
+  }
+
+  ngOnInit(): void {
+    this.calculate();
+  }
+
+  onZenPriceChange(event: any): void {
+    this.zenPrice = event.target.value;
+    // console.log();
+    this.calculate();
+  }
+
+  onZaxChange(event: any): void {
+    this.zax = event.target.value;
+    this.calculate();
+  }
+
+  onSellPriceChange(event: any): void {
+    this.sellPrice = event.target.value;
+    this.calculate();
+  }
 }
